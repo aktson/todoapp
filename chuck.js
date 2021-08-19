c0cd856657c50dcf354126a47b617dbea38de506
Ankit Soni <akt_soni@yahoo.co.in>
2021-08-19 12:03:31 +0200

    chuck norris api app finished

--- chuck.js
+++ chuck.js
@@ -13,23 +13,41 @@ function getJokes(e) {
     const number = document.querySelector("input[type=number]").value;
 
     const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            
+            let output = "";
+
+            if(response.type === "success") {
+                const clearBtn = document.createElement("button");
+                clearBtn.textContent= "Clear";
+                clearBtn.classList.add("button");
+
+                clearBtn.onclick = function () {
+                   location.reload();
+                }
+               
+                const header = document.querySelector("header");
+                header.appendChild(clearBtn);
+                response.value.forEach(function(joke) {
+               output += `<li>${joke.joke}</li>`;
+               listContainer.innerHTML = output;
+
+                })
+            } else {
+                output += "<li>Something went wrond</li>"
+            }
+         }
+    }
+
+    xhr.send();
     console.log(number);
 }
 
-function receivingFunction(arg) {
-    if (arg === typeof "function") {
-        console.log(arg);
-    }
-    else {
-        console.log ("not a function")
-    }
 
-}
 
-function callbackFunction() {
-
- console.log ("I am a callback function");
-}
-console.log(receivingFunction(callbackFunction));
-
-callbackFunction();
