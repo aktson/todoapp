1eb361f52014015f7b004e67029dd39c81a4b1b5
Ankit Soni <akt_soni@yahoo.co.in>
2021-08-25 21:49:13 +0200

    style updated, async added
    async/await method added

--- chuck.js
+++ chuck.js
@@ -1,53 +1,75 @@
 "use strict";
 const input = document.querySelector ("input");
 const addButton = document.querySelector (".add");
-const listContainer = document.querySelector (".list")
+const listContainer = document.querySelector (".list");
+const clearBtn = document.querySelector(".clear");
 
 
 addButton.addEventListener("click",  getJokes) ;
 
-
-function getJokes(e) {
-    e.preventDefault();
-
+async function getJokes() {
     const number = document.querySelector("input[type=number]").value;
+    const url = `http://api.icndb.com/jokes/random/${number}`;
 
-    const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+    const response = await fetch(url);
+    const data = await response.json();
+    const jokes = data.value;
 
-    xhr.onload = function () {
-        if (this.status === 200) {
-            const response = JSON.parse(this.responseText);
-            console.log(response);
-            
-            let output = "";
-
-            if(response.type === "success") {
-                const clearBtn = document.createElement("button");
-                clearBtn.textContent= "Clear";
-                clearBtn.classList.add("button");
-
-                clearBtn.onclick = function () {
-                   location.reload();
-                }
-               
-                const header = document.querySelector("header");
-                header.appendChild(clearBtn);
-                response.value.forEach(function(joke) {
-               output += `<li>${joke.joke}</li>`;
-               listContainer.innerHTML = output;
-
-                })
-            } else {
-                output += "<li>Something went wrond</li>"
-            }
-         }
+ 
+  
+    clearBtn.onclick = function () {
+        location.reload();
     }
 
-    xhr.send();
-    console.log(number);
+    jokes.forEach ((joke) => {
+    listContainer.innerHTML +=  `<li>${joke.joke}</li>`;
+    clearBtn.classList.remove("hidden");
+    });
 }
+
+getJokes();
+// function getJokes(e) {
+//     e.preventDefault();
+
+//     const number = document.querySelector("input[type=number]").value;
+
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+//     xhr.onload = function () {
+//         if (this.status === 200) {
+//             const response = JSON.parse(this.responseText);
+//             console.log(response);
+            
+//             let output = "";
+
+//             if(response.type === "success") {
+//                 const clearBtn = document.createElement("button");
+//                 clearBtn.textContent= "Clear";
+//                 clearBtn.classList.add("button");
+
+//                 clearBtn.onclick = function () {
+//                    location.reload();
+//                 }
+               
+//                 const header = document.querySelector("header");
+//                 header.appendChild(clearBtn);
+//                 response.value.forEach(function(joke) {
+//                output += `<li>${joke.joke}</li>`;
+//                listContainer.innerHTML = output;
+
+//                 })
+//             } else {
+//                 output += "<li>Something went wrond</li>"
+//             }
+//          }
+//     }
+
+//     xhr.send();
+//     console.log(number);
+// }
 
 
 
